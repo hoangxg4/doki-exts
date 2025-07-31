cac9614921c0c3de38615733fce69973adbcd223
dragonx943 <premieregirl26@gmail.com>
2025-07-31 17:47:52 +0700

    [HentaiVN.plus] Add author search

--- src/main/kotlin/org/dokiteam/doki/parsers/site/madara/vi/HentaiVnPlus.kt
+++ src/main/kotlin/org/dokiteam/doki/parsers/site/madara/vi/HentaiVnPlus.kt
@@ -2,9 +2,17 @@ package org.dokiteam.doki.parsers.site.madara.vi
 
 import org.dokiteam.doki.parsers.MangaLoaderContext
 import org.dokiteam.doki.parsers.MangaSourceParser
+import org.dokiteam.doki.parsers.model.ContentRating
 import org.dokiteam.doki.parsers.model.ContentType
+import org.dokiteam.doki.parsers.model.Manga
+import org.dokiteam.doki.parsers.model.MangaListFilter
 import org.dokiteam.doki.parsers.model.MangaParserSource
+import org.dokiteam.doki.parsers.model.MangaState
+import org.dokiteam.doki.parsers.model.SortOrder
 import org.dokiteam.doki.parsers.site.madara.MadaraParser
+import org.dokiteam.doki.parsers.util.oneOrThrowIfMany
+import org.dokiteam.doki.parsers.util.parseHtml
+import org.dokiteam.doki.parsers.util.urlEncoded
 
 @MangaSourceParser("HENTAIVNPLUS", "HentaiVN.plus", "vi", ContentType.HENTAI)
 internal class HentaiVnPlus(context: MangaLoaderContext) :
@@ -12,4 +20,99 @@ internal class HentaiVnPlus(context: MangaLoaderContext) :
 	override val listUrl = "truyen-hentai/"
 	override val tagPrefix = "the-loai/"
 	override val datePattern = "dd/MM/yyyy"
+	override val authorSearchSupported = true
+
+	override suspend fun getListPage(page: Int, order: SortOrder, filter: MangaListFilter): List<Manga> {
+		val pages = page + 1
+
+		val url = buildString {
+			if (!filter.author.isNullOrEmpty()) {
+				clear()
+				append("https://")
+				append(domain)
+				append("/tac-gia/")
+				append(filter.author.lowercase().replace(" ", "-"))
+
+				if (pages > 1) {
+					append("/page/")
+					append(pages.toString())
+				}
+
+				append("/?m_orderby=")
+				when (order) {
+					SortOrder.POPULARITY -> append("views")
+					SortOrder.UPDATED -> append("latest")
+					SortOrder.NEWEST -> append("new-manga")
+					SortOrder.ALPHABETICAL -> {}
+					SortOrder.RATING -> append("trending")
+					SortOrder.RELEVANCE -> {}
+					else -> append("latest") // default
+				}
+				return@buildString
+			}
+
+			append("https://")
+			append(domain)
+
+			if (pages > 1) {
+				append("/page/")
+				append(pages.toString())
+			}
+
+			append("/?s=")
+
+			filter.query?.let {
+				append(filter.query.urlEncoded())
+			}
+
+			append("&post_type=wp-manga")
+
+			if (filter.tags.isNotEmpty()) {
+				filter.tags.forEach {
+					append("&genre[]=")
+					append(it.key)
+				}
+			}
+
+			filter.states.forEach {
+				append("&status[]=")
+				when (it) {
+					MangaState.ONGOING -> append("on-going")
+					MangaState.FINISHED -> append("end")
+					MangaState.ABANDONED -> append("canceled")
+					MangaState.PAUSED -> append("on-hold")
+					MangaState.UPCOMING -> append("upcoming")
+					else -> throw IllegalArgumentException("$it not supported")
+				}
+			}
+
+			filter.contentRating.oneOrThrowIfMany()?.let {
+				append("&adult=")
+				append(
+					when (it) {
+						ContentRating.SAFE -> "0"
+						ContentRating.ADULT -> "1"
+						else -> ""
+					},
+				)
+			}
+
+			if (filter.year != 0) {
+				append("&release=")
+				append(filter.year.toString())
+			}
+
+			append("&m_orderby=")
+			when (order) {
+				SortOrder.POPULARITY -> append("views")
+				SortOrder.UPDATED -> append("latest")
+				SortOrder.NEWEST -> append("new-manga")
+				SortOrder.ALPHABETICAL -> append("alphabet")
+				SortOrder.RATING -> append("rating")
+				SortOrder.RELEVANCE -> {}
+				else -> {}
+			}
+		}
+		return parseMangaList(webClient.httpGet(url).parseHtml())
+	}
 }
