77ad11a6b1ad64812a8184a27c693a1c3d493a52
hoangxg4 <161178554+hoangxg4@users.noreply.github.com>
2025-09-03 09:17:42 +0700

    Update MangaPage.kt

--- src/main/kotlin/org/dokiteam/doki/parsers/model/MangaPage.kt
+++ src/main/kotlin/org/dokiteam/doki/parsers/model/MangaPage.kt
@@ -1,6 +1,5 @@
 package org.dokiteam.doki.parsers.model
 
-import okhttp3.Headers
 import org.dokiteam.doki.parsers.MangaParser
 
 public data class MangaPage(
@@ -8,5 +7,4 @@ public data class MangaPage(
 	@JvmField public val url: String,
 	@JvmField public val preview: String?,
 	@JvmField public val source: MangaSource,
-	@JvmField public val headers: Headers? = null
 )
