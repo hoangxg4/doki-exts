62a8d2276db8bf2074ee19851360881b03c24ff6
hoangxg4 <161178554+hoangxg4@users.noreply.github.com>
2025-10-10 08:56:38 +0700

    Update and rename build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import tasks.ReportGenerateTask
+
+plugins {
+    `java-library`
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    `maven-publish`
+}
+
+group = "org.dokiteam"
+version = "1.0"
+
+ksp {
+    arg("summaryOutputDir", "${project.projectDir}/.github")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += listOf(
+            "-opt-in=kotlin.RequiresOptIn",
+            "-opt-in=kotlin.contracts.ExperimentalContracts",
+            "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+            "-opt-in=org.dokiteam.doki.parsers.InternalParsersApi",
+        )
+    }
+}
+
+kotlin {
+    jvmToolchain(8)
+    explicitApi()
+    sourceSets.main.get().kotlin.srcDirs("build/generated/ksp/main/kotlin")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    api(libs.jsoup)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp)
+    implementation(libs.okio)
+    implementation(libs.json)
+    implementation(libs.androidx.collection)
+    implementation(libs.nanohttpd)
+
+    // KSP dependency, lưu ý cách tham chiếu project
+    ksp(project(":doki-ksp"))
+
+    // Test dependencies - sử dụng bundle để thêm cả 3 thư viện JUnit
+    testImplementation(libs.bundles.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.quickjs)
+}
+
+tasks.register<ReportGenerateTask>("generateTestsReport")
